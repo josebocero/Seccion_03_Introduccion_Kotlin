265cf9d94461938e1aaeabc7efb81df03ff78941
josebocero <josebocero@gmail.com>
2021-06-27 00:55:03 +0200

    video 31-3 Classes

--- app/src/main/java/jose_g_bocero/com/jose/seccion_03_introduccion_kotlin/MainActivity.kt
+++ app/src/main/java/jose_g_bocero/com/jose/seccion_03_introduccion_kotlin/MainActivity.kt
@@ -2,10 +2,7 @@ package jose_g_bocero.com.jose.seccion_03_introduccion_kotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import others.ControlFlow
-import others.Nullable
-import others.Operators
-import others.Variables
+import others.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +13,9 @@ class MainActivity : AppCompatActivity() {
         Nullable() // Si llamamos al showCases tendremos NPE
         Operators().showCases()
         ControlFlow().showCases()
+        AccessModifiers()
+        Interfaces().showCases()
+        Classes().showCaseS()
+        Functions().showCases()
     }
 }
